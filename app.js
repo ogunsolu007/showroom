d1d54817e9567f255819d0d292b28d802cd0a6f9
ogunsolu007 <63315246+ogunsolu007@users.noreply.github.com>
2020-12-27 10:27:05 +0100

    room

---
+++ app.js
@@ -0,0 +1,44 @@
+const Slide = document.querySelector(".head-slide");
+const contentSlide = document.querySelectorAll(".content");
+
+//button
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+//couter
+
+let counter = 1;
+
+const size = contentSlide[0].clientWidth;
+console.log(size);
+
+Slide.style.transform = `translateX(${-size * counter}px)`;
+
+//btn listener
+
+nextBtn.addEventListener("click", () => {
+  if (counter >= contentSlide.length - 1) return;
+  Slide.style.transition = "transform 2s ease-in-out";
+  counter++;
+  Slide.style.transform = `translateX(${-size * counter}px)`;
+});
+
+prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return;
+  Slide.style.transition = "transform 2s ease-in-out";
+  counter--;
+  Slide.style.transform = `translateX(${-size * counter}px)`;
+});
+
+Slide.addEventListener("transitionend", () => {
+  if (contentSlide[counter].id === "lastClone") {
+    Slide.style.transition = "none";
+    counter = contentSlide.length - 2;
+    Slide.style.transform = `translateX(${-size * counter}px)`;
+  }
+  if (contentSlide[counter].id === "firstClone") {
+    Slide.style.transition = "none";
+    counter = contentSlide.length - counter;
+    Slide.style.transform = `translateX(${-size * counter}px)`;
+  }
+});
